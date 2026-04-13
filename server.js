34677163b81b1ae4cebf0c8acda326972cd9d099
kokohtet816145-droid <kokohtet816145-droid@users.noreply.github.com>
2026-04-14 06:23:45 +0630

    add chat server files

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+    socket.on('chat message', (msg) => {
+        console.log('Message: ' + msg);
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
